20c6626211e256a55a50c01de75650f457d5aa1d
GeraldSalazar <geraldsalazar4@gmail.com>
2023-03-23 23:08:46 -0600

    intitial service with users data

--- WebServer/src/controllers/auth.controller.js
+++ WebServer/src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
+const usersService = require('../services/auth-service.service')
+
 async function authenticateUser(req, res, next){
     console.log('Got it!')
     console.log(req.body)
+    console.log(usersService.getUsersData())
     try {
         res.json({"userHasAccess": false});
     } catch (err) {
